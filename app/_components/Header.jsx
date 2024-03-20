76521b024cc4759807d82c0d62a6a03e7ad8d8ba
Ibrahim Hamdy <ibrhmhmdy@gmail.com>
2024-03-20 16:43:16 +0200

    Create ProductList Component

---
+++ app/_components/Header.jsx
@@ -0,0 +1,98 @@
+import Image from 'next/image'
+import React from 'react'
+
+function Header() {
+  return (
+    <header className="bg-white dark:bg-gray-900">
+        <div className="mx-auto flex h-16 items-center justify-between gap-8 px-4 sm:px-6 lg:px-8 shadow-md">
+            <Image src='/logo.svg' width={50} height={50} alt='logo image ecommerce nextjs'/>
+            <div className='flex flex-1 items-center justify-end md:justify-between'>
+                <nav aria-label="Global" className="hidden md:block">
+                    <ul className="flex items-center gap-6 text-sm">
+                        <li>
+                            <a
+                                className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                                href="/"
+                            >Home</a>
+                        </li>
+
+                        <li>
+                            <a
+                                className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                                href="#explore"
+                            >
+                                Explore
+                            </a>
+                        </li>
+
+                        <li>
+                            <a
+                                className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                                href="#projects"
+                            >
+                                Projects
+                            </a>
+                        </li>
+
+                        <li>
+                            <a
+                                className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                                href="#aboutus"
+                            >
+                                About Us
+                            </a>
+                        </li>
+
+                        <li>
+                            <a
+                                className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                                href="#contactus"
+                            >
+                                Contact Us
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div className="flex items-center gap-4">
+                <div className="sm:flex sm:gap-4">
+                    <a
+                        className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow dark:hover:bg-teal-500"
+                        href="#"
+                    >
+                        Login
+                    </a>
+
+                    <div className="hidden sm:flex">
+                        <a
+                        className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600 dark:bg-gray-800 dark:text-white dark:hover:text-white/75"
+                        href="#"
+                        >
+                        Register
+                        </a>
+                    </div>
+                </div>
+
+                <div className="block md:hidden">
+                    <button
+                        className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75 dark:bg-gray-800 dark:text-white dark:hover:text-white/75"
+                    >
+                        <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-5 w-5"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        >
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header

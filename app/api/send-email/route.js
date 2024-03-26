bdc3a7db94c3a4e0e5da8e515e3f7b5590e44918
Ibrahim Hamdy <ibrhmhmdy@gmail.com>
2024-03-26 17:46:54 +0200

    Fix and Improved with Finish Project

--- app/api/send-email/route.js
+++ app/api/send-email/route.js
@@ -1,14 +1,14 @@
+import { useContext } from 'react';
 import { EmailTemplate } from '../../_components/EmailTemplate';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST() {
-
   try {
     const data = await resend.emails.send({
       from: 'onboarding@resend.dev',
-      to: ["ibrhmhmdy@gmail.com"],
+      to: [user?.primaryEmailAddress.emailAddress],
       subject: 'Hello Mohammed',
       react: EmailTemplate({ firstName: 'Mohammed' }),
     });
